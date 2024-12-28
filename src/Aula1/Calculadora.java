213506a18e4996905ca062512031f78b5ed4091c
Não Sei Programar (Files) <eunaoseiprogramarnada@gmail.com>
2024-12-28 13:30:57 -0300

    Create Calculadora.java

---
+++ src/Aula1/Calculadora.java
@@ -0,0 +1,27 @@
+package Aula1;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro numero: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo numero: ");
+        double numero2 = scanner.nextDouble();
+
+        double soma = numero1 + numero2;
+        double subtracao = numero1 - numero2;
+        double multiplicacao = numero1 * numero2;
+        double divisao = numero1 / numero2;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisão: " + divisao);
+
+        scanner.close();
+    }
+}
